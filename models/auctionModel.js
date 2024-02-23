12c9c882643a80d59c3a15c32d9ead21b2e96d07
Toby <109319639+CyberWolf101@users.noreply.github.com>
2024-02-23 16:30:17 +0100

    Update auctionModel.js

--- models/auctionModel.js
+++ models/auctionModel.js
@@ -1,137 +1,68 @@
 const mongoose = require('mongoose');
 
-const auctionSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: true,
-  },
-  created_at: {
-    type: Number,
-    required: true,
-  },
-  product_id: {
-    type: String,
-    required: true,
-  },
-  auctionId: {
-    type: String,
-    required: true,
-  },
-  acceptedFor: {
-    type: String,
-    default: ''
-  },
-
-  accepted_amount: {
-    type: Number,
-  },
-
-  categories: {
-    type: [String],
-    required: true,
-  },
-  auctioner_email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  userID: {
-    type: String,
-    required: true,
-  },
-  product_url: {
-    type: String,
-    required: true,
-  },
-  product_url1: {
-    type: String,
-    default: '',
-  },
-  product_url2: {
-    type: String,
-    default: '',
-  },
-  product_url3: {
-    type: String,
-    default: '',
-  },
-  isOutOfStock: {
+const productSchema = new mongoose.Schema({
+  productName: String,
+  auctionId: String,
+  created_at: Number,
+  product_id: String,
+  amount: Number,
+  categories: [String],
+  endDate: Number,
+  price: Number,
+  offPrice: Number,
+  auctionPrice: Number,
+  description: String,
+  shop: String,
+  product_url: String,
+  auctionType: String,
+  imageUrls: [String],
+  publicIds: [String],
+  product_url1: String,
+  product_url2: String,
+  product_url3: String,
+  parent_Product_Id: String,
+  isOutOfStock: Boolean,
+  isAuctioned: {
     type: Boolean,
     default: false,
   },
-  bids: {
+  isPromoted: {
+    type: Boolean,
+    default: false,
+  },
+  promotEnd: {
     type: Number,
     default: 0,
   },
-  highestBid: {
+  promorStart: {
     type: Number,
     default: 0,
   },
-  biders: {
-    type: [],
-    default: [],
-  },
-  publicIds: {
-    type: [],
-    default: [],
-  },
-  images: {
-    type: [],
-    default: [],
-  },
-  canAccept: {
-    type: Boolean,
-    default: true,
-  },
-  endDate: {
-    type: Number,
+  promoStart: Number,
+  sizes: {
+    type: Object,
     required: true,
+    default: {},
   },
-  auctionType: {
-    type: String,
+  subAdminID: String,
+  shopID: String,
+  discountPrice: Number,
+  keyFeatures: {
+    type: Array,
+    default: [],
+  },
+  variations: {
+    type: Array,
+    default: [],
+  },
+
+  numericSizeObject: {
+    type: Object,
     required: true,
+    default: {},
   },
-  auctionId: {
-    type: String,
-  },
-  parent_Product_Id: {
-    type: String,
-  },
-  not_visible: {
-    type: Boolean,
-    default: false
-  },
-  user_that_paid_id: {
-    type: String,
-  },
-  auction_Item_Paid_For: {
-    type: String,
-  },
-  amountPaid_by_user: {
-    type: Number,
-  },
-  hasPaid: {
-    type: Boolean
-  },
-  isUserAuction: {
-    type: Boolean
-  },
-  auctionerPhone: {
-    type: String
-  }
 });
 
-const Auction = mongoose.model('Auction', auctionSchema);
+const Product = mongoose.model('Product', productSchema);
 
-module.exports = Auction;
+module.exports = Product;
